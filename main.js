01154369c094f52b9de8ef6b74dc2cf8af0837b3
computershawn <shontek@gmail.com>
2025-02-24 23:08:11 -0800

    Clean up unused code

--- main.js
+++ main.js
@@ -18,7 +18,6 @@ async function pasteSelection() {
         modalBehavior: 'execute',
       }
     );
-    // console.log('Area pasted to new document');
   } catch (err) {
     console.error('Paste operation error:', err);
   }
@@ -34,7 +33,6 @@ async function createSelection({ top, left, bottom, right }) {
       { top, left, bottom, right },
       constants.SelectionType.REPLACE
     );
-    // console.log('Area selected');
   } catch (err) {
     console.error('Select operation error:', err);
   }
@@ -55,7 +53,6 @@ async function copySelection() {
         modalBehavior: 'execute',
       }
     );
-    // console.log('Area copied from active document');
   } catch (err) {
     console.error('Copy operation error:', err);
   }
@@ -132,18 +129,14 @@ const getParams = (saveFile, documentID) => ({
       as: {
         _obj: 'JPEGFormat',
         quality: 12, // Max quality
-        // embedColorProfile: true,
         formatOptions: {
           _obj: 'JPEGFormatOptions',
           quality: 12,
-          // scans: 3,
-          // matte: {_enum: "matteType", _value: "none"}
         },
       },
       in: { _path: saveFile },
       saveStage: { _enum: 'saveStageType', _value: 'saveBegin' },
       documentID,
-      // copy: true, // Save a copy
       lowerCase: true,
       saveAs: true,
     },
@@ -154,27 +147,6 @@ const getParams = (saveFile, documentID) => ({
   },
 });
 
-// const pages = 1; // 3;
-// const pageCols = 6; // 3;
-// const pageRows = 6; // 3;
-// const cols = 6; // 6;
-
-// const xOff = 90;
-// const yOff = 240;
-// const xGap = 11;
-// const yGap = 310;
-// const wd = 515;
-// const ht = 515;
-
-// // xOff: 90, yOff: 240, xGap: 11, yGap: 310, wd: 515, ht: 515, rows: 2, cols: 6
-
-// const bounds = getBounds({ xOff, yOff, xGap, yGap, wd, ht, pageRows, pageCols });
-// console.log(bounds);
-// const frameNums = generateFrameNums({ pages, pageCols, pageRows, cols });
-// console.log(frameNums);
-
-// const frameNums = generateFrameNums();
-
 const doEverything = async () => {
   if (!window.require('photoshop').app.activeDocument) {
     throw new Error('No active document!');
@@ -182,29 +154,6 @@ const doEverything = async () => {
 
   const { localFileSystem: fileSys } = require('uxp').storage;
   const folderRef = await fileSys.getFolder();
-
-  // const pages = 1; // 3;
-  // const pageCols = 6; // 3;
-  // const pageRows = 6; // 3;
-  // const cols = 6; // 6;
-
-  // const xOff = 90;
-  // const yOff = 240;
-  // const xGap = 11;
-  // const yGap = 310;
-  // const wd = 515;
-  // const ht = 515;
-
-  // const pages = 20; // 3;
-  // const pageCols = 3; // 3;
-  // const pageRows = 3; // 3;
-  // const cols = 6; // 6;
-  // const xOff = 9;
-  // const yOff = 106;
-  // const xGap = 42;
-  // const yGap = 80;
-  // const wd = 1700;
-  // const ht = 2210;
 
   const {
     pageRows,
@@ -217,34 +166,7 @@ const doEverything = async () => {
     height: ht,
     yGap,
     pages,
-    pageNum,
   } = getValues();
-
-  // const pageRows = 3; // 3;
-  // const pageCols = 3; // 3;
-  // const cols = 6; // 6;
-  // const xOff = 9;
-  // const wd = 1700;
-  // const xGap = 42;
-  // const yOff = 106;
-  // const ht = 2210;
-  // const yGap = 80;
-  // const pages = 20; // 3;
-  // const pageNum = 1;
-
-  // console.log(values.pageRows === pageRows);
-  // console.log(values.pageCols === pageCols);
-  // console.log(values.expanded === cols);
-  // console.log(values.xOffset === xOff);
-  // console.log(values.width === wd);
-  // console.log(values.xGap === xGap);
-  // console.log(values.yOffset === yOff);
-  // console.log(values.height === ht);
-  // console.log(values.yGap === yGap);
-  // console.log(values.pages === pages);
-  // console.log(values.pageNum === pageNum);
-
-  // xOff: 90, yOff: 240, xGap: 11, yGap: 310, wd: 515, ht: 515, rows: 2, cols: 6
 
   const bounds = getBounds({
     xOff,
@@ -259,8 +181,8 @@ const doEverything = async () => {
 
   const frameNums = generateFrameNums({ pages, pageCols, pageRows, cols });
 
+  const pageNum = 1;
   const indices = frameNums[pageNum - 1];
-  // const timestamp = Date.now();
 
   try {
     for (let i = 0; i < indices.length; i++) {
@@ -273,14 +195,14 @@ const doEverything = async () => {
 
       const newFile = await folderRef.createFile(`frames-${indices[i]}.jpg`);
       const saveFile = await fileSys.createSessionToken(newFile);
-      await saveThing(saveFile);
+      await exportFile(saveFile);
     }
   } catch (err) {
     console.error('Error:', err);
   }
 };
 
-const saveThing = async (saveFileRef) => {
+const exportFile = async (saveFileRef) => {
   const { app, action } = require('photoshop');
   const documentID = app.activeDocument.id;
   const params = getParams(saveFileRef, documentID);
@@ -289,22 +211,10 @@ const saveThing = async (saveFileRef) => {
     await action.batchPlay(params.commands, params.options);
     app.activeDocument.closeWithoutSaving();
 
-    // console.log('Saved JPEG successfully');
   } catch (err) {
     console.error('Save failed:', err);
   }
 };
-
-document.getElementById('btnPrev').addEventListener('click', () => {
-  const elem = document.getElementById('pageNum');
-  const temp = Math.max(1, parseInt(elem.value) - 1);
-  elem.value = `${temp}`;
-});
-
-document.getElementById('btnNext').addEventListener('click', () => {
-  const elem = document.getElementById('pageNum');
-  elem.value = `${parseInt(elem.value) + 1}`;
-});
 
 function getValues() {
   const varbs = [
@@ -318,7 +228,6 @@ function getValues() {
     'height',
     'yGap',
     'pages',
-    'pageNum',
   ];
   const temp = varbs.reduce((prev, curr) => {
     const val = document.getElementById(curr).value;
@@ -330,34 +239,6 @@ function getValues() {
 
   return temp;
 }
-
-// document.getElementById('btnExport').addEventListener('click', () => {
-//   const values = getValues();
-
-//   const pageRows = 3; // 3;
-//   const pageCols = 3; // 3;
-//   const cols = 6; // 6;
-//   const xOff = 9;
-//   const wd = 1700;
-//   const xGap = 42;
-//   const yOff = 106;
-//   const ht = 2210;
-//   const yGap = 80;
-//   const pages = 20; // 3;
-//   const pageNum = 1;
-
-//   console.log(values.pageRows === pageRows);
-//   console.log(values.pageCols === pageCols);
-//   console.log(values.expanded === cols);
-//   console.log(values.xOffset === xOff);
-//   console.log(values.width === wd);
-//   console.log(values.xGap === xGap);
-//   console.log(values.yOffset === yOff);
-//   console.log(values.height === ht);
-//   console.log(values.yGap === yGap);
-//   console.log(values.pages === pages);
-//   console.log(values.pageNum === pageNum);
-// });
 
 function getPageLayerNames() {
   const { app } = require('photoshop');
@@ -401,24 +282,24 @@ function toggleLayers(layerName, showAll) {
   }
 }
 
-document.getElementById('btnExport').addEventListener('click', () => {
-  require('photoshop').core.executeAsModal(
-    () => {
-      const pageLayerNames = getPageLayerNames();
-      pageLayerNames.forEach((layerName) => {
-        toggleLayers(layerName);
-      });
-      toggleLayers('', true);
-    },
-    {
-      commandName: 'Generic name of the command',
-    }
-  );
-});
-
-// Batch export individual frames to JPG
 // document.getElementById('btnExport').addEventListener('click', () => {
-//   require('photoshop').core.executeAsModal(doEverything, {
-//     commandName: 'Generic name of the command',
-//   });
+//   require('photoshop').core.executeAsModal(
+//     () => {
+//       const pageLayerNames = getPageLayerNames();
+//       pageLayerNames.forEach((layerName) => {
+//         toggleLayers(layerName);
+//       });
+//       toggleLayers('', true);
+//     },
+//     {
+//       commandName: 'Generic name of the command',
+//     }
+//   );
 // });
+
+// Batch export individual frames to JPEGs
+document.getElementById('btnExport').addEventListener('click', () => {
+  require('photoshop').core.executeAsModal(doEverything, {
+    commandName: 'Generic name of the command',
+  });
+});
